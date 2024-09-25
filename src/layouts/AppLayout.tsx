c6675ae5614b7554d24e264e71acaf8652ea3105
Dante Tárraga <dante.tarragau@gmail.com>
2024-09-25 17:18:14 -0500

    Agregar navegación y vista para crear proyectos, incluyendo componentes de menú y mejoras en la vista del tablero

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Logo from "../components/Logo"
+import NavMenu from "@/components/NavMenu"
 
 const AppLayout = () => {
   return (
@@ -9,6 +10,8 @@ const AppLayout = () => {
           <div className="w-64"> 
             <Logo />
           </div>
+
+          <NavMenu />
         </div>
       </header>
 
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom"
 
-const DashboardView = () => {
+const CreateProjectView = () => {
   return (
     <>
-      <h1 className="text-5xl font-black">My Projects</h1>
+      <h1 className="text-5xl font-black">Create Project</h1>
       <p className="text-2xl font-light text-gray-500 mt-5">
-        Here you can manage your projects
+        Fill the form to create a new
       </p>
 
       <nav className="my-5">
         <Link 
           className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-md" 
-          to='/projects/create'
+          to='/'
         >
-          Create Project
+          Return to Dashboard
         </Link>
       </nav>    
     </>
   )
 }
 
-export default DashboardView
+export default CreateProjectView
