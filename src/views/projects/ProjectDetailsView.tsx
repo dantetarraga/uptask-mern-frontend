84b6aa8a766e258f356d910bbe03427dd2c1ade8
Dante Tárraga <dante.tarragau@gmail.com>
2024-10-03 16:25:54 -0500

    feat: agregar funcionalidad para obtener detalles del proyecto mediante ID usando react-query

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -1,4 +1,17 @@
+import { getProjectById } from "@/api/apiProject"
+import { useQuery } from "@tanstack/react-query"
+import { useParams } from "react-router-dom"
+
 const ProjectDetailsView = () => {
+  const params = useParams()
+  const projectId = params.projectId as string
+
+  const {data, isLoading, isError} = useQuery({
+    queryKey: ['editProject', projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false
+  })
+
   return (
     <div>ProjectDetailsView</div>
   )
